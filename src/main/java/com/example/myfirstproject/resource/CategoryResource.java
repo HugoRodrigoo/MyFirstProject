6ce1a7023368292387736cea6b7bc7668c2a35d1
HugoRodrigo <hugorbarradas@gmail.com>
2022-06-16 23:51:44 -0300

    Category memory

---
+++ src/main/java/com/example/myfirstproject/resource/CategoryResource.java
@@ -0,0 +1,35 @@
+package com.example.myfirstproject.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.myfirstproject.entities.Category;
+import com.example.myfirstproject.repositories.CategoryRepository;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = repository.findAll();
+		return ResponseEntity.ok().body(list);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category res = repository.findById(id);
+		return ResponseEntity.ok().body(res);
+	}
+
+}
